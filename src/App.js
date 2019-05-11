ff7fbac7dfb8e2592b5b9359331e9bda215bd251
Brent P. Newhall <brent@brentnewhall.com>
2019-05-11 16:34:54 -0400

    Add button colors, question, and screenshot 2

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
       promptIndex: 0,
       prompt: {},
       answers: [],
+      buttonColor: ['cyan','cyan','cyan','cyan'],
+      correctAnswer: 0,
+      question: "What's wrong here?",
     }
     this.setPrompt = this.setPrompt.bind( this );
     this.answerClicked = this.answerClicked.bind( this );
@@ -25,12 +28,37 @@ class App extends Component {
       this.setState( {
         screenshot: '/images/screenshots/' + prompts[this.state.promptIndex].image,
         answers: prompts[this.state.promptIndex].answers,
+        correctAnswer: prompts[this.state.promptIndex].correctAnswer,
       });
     }
   }
 
   answerClicked( answer ) {
     console.log( "Clicked on", answer );
+    let colors = [];
+    for( let i = 0; i < 4; i++ ) {
+      if( i === this.state.correctAnswer ) {
+        colors.push( 'green' );
+      }
+      else if( i === answer ) {
+        colors.push( 'cyan' );
+      }
+      else {
+        colors.push( 'grey' );
+      }
+    }
+    if( answer === this.state.correctAnswer ) {
+      this.setState( {
+        question: "That's right!",
+        buttonColor: colors,
+      })
+    }
+    else {
+      this.setState( {
+        question: "Nope!",
+        buttonColor: colors,
+      })
+    }
   }
 
   render() {
@@ -43,14 +71,14 @@ class App extends Component {
         <footer className="container">
         <div className="row">
           <div className="col m12 question">
-            What's wrong here?
+            {this.state.question}
           </div>
         </div>
         <div className="row">
-          <button className="btn col m6" onClick={() => this.answerClicked(0)}>{this.state.answers[0]}</button>
-          <button className="btn col m6" onClick={() => this.answerClicked(1)}>{this.state.answers[1]}</button>
-          <button className="btn col m6" onClick={() => this.answerClicked(2)}>{this.state.answers[2]}</button>
-          <button className="btn col m6" onClick={() => this.answerClicked(3)}>{this.state.answers[3]}</button>
+          <button className={"btn col m6 " + this.state.buttonColor[0]} onClick={() => this.answerClicked(0)}>{this.state.answers[0]}</button>
+          <button className={"btn col m6 " + this.state.buttonColor[1]} onClick={() => this.answerClicked(1)}>{this.state.answers[1]}</button>
+          <button className={"btn col m6 " + this.state.buttonColor[2]} onClick={() => this.answerClicked(2)}>{this.state.answers[2]}</button>
+          <button className={"btn col m6 " + this.state.buttonColor[3]} onClick={() => this.answerClicked(3)}>{this.state.answers[3]}</button>
         </div>
         </footer>
       </div>
--- src/prompts.js
+++ src/prompts.js
@@ -6,8 +6,19 @@ export const prompts = [
             'Too many closing curly braces',
             'Bad function definition',
             'Function name misspelled',
-        ]
-    }
+        ],
+        correctAnswer: 0,
+    },
+    {
+        image: 'screenshot0.png',
+        answers: [
+            'Needs to close a curly brace',
+            'Too many closing curly braces',
+            'Needs comma',
+            'Should be setState() instead of this.state =',
+        ],
+        correctAnswer: 2,
+    },
 ];
 
 export default prompts;
